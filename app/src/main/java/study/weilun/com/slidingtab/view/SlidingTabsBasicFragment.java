a3090fabc8a07a38b91d081d7a24f2fe3cf82db7
weilun <weilun@staff.weibo.com>
2015-05-08 16:21:44 +0800

    添加需要显示的Fragment

---
+++ app/src/main/java/study/weilun/com/slidingtab/view/SlidingTabsBasicFragment.java
@@ -0,0 +1,73 @@
+package study.weilun.com.slidingtab.view;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import study.weilun.com.slidingtab.R;
+import study.weilun.com.slidingtab.layout.SlidingTabLayout;
+
+/**
+ * Created by weilun on 2015/5/8.
+ */
+public class SlidingTabsBasicFragment extends Fragment {
+
+    private SlidingTabLayout mSlidingTabLayout ;
+
+    private ViewPager mViewPager ;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_sample, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        mViewPager = (ViewPager) view.findViewById(R.id.viewpager);
+        mViewPager.setAdapter(new SimpleAdapter());
+
+        mSlidingTabLayout = (SlidingTabLayout) view.findViewById(R.id.sliding_tabs);
+        mSlidingTabLayout.setViewPager(mViewPager);
+    }
+
+    private class SimpleAdapter extends PagerAdapter {
+        @Override
+        public int getCount() {
+            return 10;
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object o) {
+            return o == view;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return "Item " + position;
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            View view = getActivity().getLayoutInflater().inflate(R.layout.pager_item, container, false);
+            container.addView(view);
+
+            TextView title = (TextView) view.findViewById(R.id.title);
+            title.setText(String.valueOf(position + 1 ));
+            return view;
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            container.removeView((View)object);
+        }
+    }
+}
